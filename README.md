8f5ab68d62b84bc97bfba46d0faddd6d676a0d2f
phever <phever@users.noreply.github.com>
2022-06-07 14:34:39 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Codewars Template
+### Easy-to-clone repo to start solving Katas within an IDE
+
+ 1. `git clone git@github.com:phever/codewars_template.git`
+ 2. Open in PyCharm (or just clone if from PyCharm?)
+ 3. ???
+ 4. Profit!
--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import tests
-
-
+# USING https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/python
+# AS THE PRELOADED KATA
 def distinct(seq):
     # Use a breakpoint in the code line below to debug your script.
     pass
@@ -12,6 +11,8 @@ def distinct(seq):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    tests.fixed_tests()
+    print("Running tests! Output is a little condensed, but "
+          "should be readable")
+    import tests
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
